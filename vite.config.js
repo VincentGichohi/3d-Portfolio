b8c6827811fc918cbb0f3f1f38f1646180b490eb
VincentGichohi <vincentegichohi@gmail.com>
2023-04-23 01:39:32 +0300

    update source code

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "https://github.com/VincentGichohi/3d-Portfolio.git",
+  build: {
+    chunkSizeWarningLimit: 2000, // set the chunk size limit to 1000
+  }
+});
